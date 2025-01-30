9cd204527a6deddd89c4d87cccd2c60f84579dd1
Ed Marsh <techwriter@edmarsh.com>
2025-01-30 16:50:55 -0500

    meh

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,28 @@
 const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const markdownIt = require("markdown-it");
+const md = new markdownIt();
+
 module.exports = function (eleventyConfig) {
+  // Add markdown filter
+  eleventyConfig.addFilter("markdown", function(value) {
+    if (!value) return '';
+    return md.render(value);
+  });
+
+  // Add plugins
+  eleventyConfig.addPlugin(syntaxHighlightPlugin);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // Add shortcodes
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // Add passthrough copies
   eleventyConfig.addPassthroughCopy("_includes/css/base.css");
   eleventyConfig.addPassthroughCopy("_includes/css/tw.css");
   eleventyConfig.addPassthroughCopy("assets/images");
-  eleventyConfig.addPlugin(syntaxHighlightPlugin);
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  // Add collections
   eleventyConfig.addCollection("skills", function (collectionApi) {
     return collectionApi
       .getAll()
@@ -28,9 +44,15 @@ module.exports = function (eleventyConfig) {
       .filter((item) => item.data.tags && item.data.tags.includes("examples"));
   });
 
+  // Return directory configuration LAST
+  return {
+    dir: {
+      input: "content",
+      includes: "../_includes",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["md", "njk"]
+  };
 };
-dir: {
-  input: "content";
-  includes: "_includes";
-  output: "_site";
-}
