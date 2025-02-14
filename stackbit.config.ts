a3cfc352a9c44027622af58d601ca2f17b3817ac
Ed Marsh <techwriter@edmarsh.com>
2025-02-14 13:48:43 -0500

    trying to get netlify visual editor working

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,13 +1,9 @@
-import { defineStackbitConfig } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
+// stackbit.config.ts
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "eleventy",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  stackbitVersion: "~0.6.0",
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
@@ -29,3 +25,4 @@ export default defineStackbitConfig({
       }
     })
   ]
+});
