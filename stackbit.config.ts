2251fb613d1546afe21fd512885cb6d11240eccc
Ed Marsh <techwriter@edmarsh.com>
2025-02-14 13:59:55 -0500

    stackbit.ts update

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,5 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from '@stackbit/cms-git';
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
@@ -7,3 +8,24 @@ export default defineStackbitConfig({
     "contentSources": [],
     "postInstallCommand": "npm i --no-save @stackbit/types"
 })
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+      assetsConfig: {
+        referenceType: "static",
+        staticDir: "assets",
+        uploadDir: "images",
+        publicPath: "/"
+      }
+    })
+  ]
