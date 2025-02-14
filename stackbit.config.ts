3d033dcb517c7f45e2dd94cdf7c6fcafbef8d886
Ed Marsh <techwriter@edmarsh.com>
2025-02-14 14:05:16 -0500

    stackbit

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,5 +1,4 @@
 import { defineStackbitConfig } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
