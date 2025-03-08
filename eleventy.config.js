707ead794e54410c8516f0d96e4ff3e8fe995941
Ed Marsh <techwriter@edmarsh.com>
2025-03-08 17:56:04 -0500

    mvp rss feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const timeToRead = require('eleventy-plugin-time-to-read');
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
 
 // Create markdown-it instance with custom configuration
@@ -40,6 +41,23 @@ module.exports = function (eleventyConfig) {
 });
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(timeToRead);
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "rss",
+    outputPath: "/feed.xml",
+    collection: {
+        name: "podcasts",
+        limit: 0,     // 0 means no limit
+    },
+    metadata: {
+        language: "en",
+        title: "Content Content podcast",
+        subtitle: "Ed Marsh interviews professionals in technical communication, content strategy, content marketing, information architecture, and others who create, organize, and maintain content online.",
+        base: "https://edmar.sh/podcasts",
+        author: {
+            name: "Ed Marsh",
+        }
+    }
+});
 
   // Add shortcodes
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
