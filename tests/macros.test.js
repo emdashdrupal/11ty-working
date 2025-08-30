e1297f195b4af1969c162459e514c6fa31cfd936
Ed Marsh <techwriter@edmarsh.com>
2025-08-30 12:28:11 -0400

    dueling AIs

--- tests/macros.test.js
+++ tests/macros.test.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-// Test that all required macro files exist
-describe('All macro files exist', () => {
-  it('should have macros.njk file', () => {
+describe('Macros file', () => {
+  test('macros.njk file should exist', () => {
     const macrosPath = path.join(__dirname, '../_includes/layouts/partials/macros.njk');
     expect(fs.existsSync(macrosPath)).toBe(true);
   });
 });
 
-// Test that all templates that use macros exist
 describe('All templates using macros exist', () => {
-  it('should have all required template files', () => {
+  test('should have all required template files', () => {
     const templatesUsingMacros = [
       '../_includes/layouts/details.njk',
       '../_includes/layouts/index.njk',
@@ -26,18 +24,13 @@ describe('All templates using macros exist', () => {
   });
 });
 
-// Test that macros file contains all required macros
 describe('Macros file contains all required macros', () => {
-  it('should contain all required macros', () => {
+  test('should contain all required macros', () => {
     const macrosPath = path.join(__dirname, '../_includes/layouts/partials/macros.njk');
     const macrosContent = fs.readFileSync(macrosPath, 'utf8');
 
     const requiredMacros = [
-      'getSortedCollection',
-      'getNavItemClasses',
-      'renderHeroImage',
       'featuredSection',
-      'renderRelatedContent',
       'dropdown',
       'listItem',
       'gridItem',
@@ -51,26 +44,22 @@ describe('Macros file contains all required macros', () => {
   });
 });
 
-// Test that templates import the macros they use
 describe('Templates import the macros they use', () => {
-  it('should import macros correctly', () => {
+  test('should import macros correctly', () => {
     const templates = {
-      '../_includes/layouts/details.njk': ['renderHeroImage', 'renderRelatedContent'],
-      '../_includes/layouts/index.njk': ['featuredSection'],
-      '../_includes/layouts/grid.njk': ['getSortedCollection'],
-      '../_includes/layouts/partials/next-previous.njk': ['getSortedCollection']
+      '../_includes/layouts/partials/navbar.njk': ['dropdown', 'listItem']
     };
 
     Object.entries(templates).forEach(([templatePath, macros]) => {
       const fullPath = path.join(__dirname, templatePath);
       const templateContent = fs.readFileSync(fullPath, 'utf8');
 
-      // Check for import statement
-      expect(templateContent.includes('from "layouts/partials/macros.njk"')).toBe(true);
+      // Check for import statement with correct path
+      expect(templateContent.includes('{% from "layouts/partials/macros.njk"')).toBe(true);
 
-      // Check for each macro
+      // Check for each macro usage
       macros.forEach(macroName => {
-        expect(templateContent.includes(`import ${macroName}`)).toBe(true);
+        expect(templateContent.includes(macroName)).toBe(true);
       });
     });
   });
